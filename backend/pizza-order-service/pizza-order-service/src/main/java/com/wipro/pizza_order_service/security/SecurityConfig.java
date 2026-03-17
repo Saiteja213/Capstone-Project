96feda34dcf576cc21c7bf6a9009bac970bd5685
Saiteja213 <21eg109b28@anurag.edu.in>
2026-03-17 16:28:38 +0530

    changes

--- backend/pizza-order-service/pizza-order-service/src/main/java/com/wipro/pizza_order_service/security/SecurityConfig.java
+++ backend/pizza-order-service/pizza-order-service/src/main/java/com/wipro/pizza_order_service/security/SecurityConfig.java
@@ -27,21 +27,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // IMPORTANT
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
-            		.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            		.requestMatchers("/actuator/**").permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .requestMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
     // CORS configuration
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
